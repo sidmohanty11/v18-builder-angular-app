8aa164d8da30031b239348127d3c4a88eff252ec
sidmohanty11 <sidmohanty11@gmail.com>
2024-09-24 17:08:16 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  BuilderContent,
+  fetchOneEntry,
+  Content,
+} from '@builder.io/sdk-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: ` <div>
+    @if (content) {
+    <content-variants
+      [model]="model"
+      [content]="content"
+      [apiKey]="apiKey"
+    ></content-variants>
+    }
+  </div>`,
+  imports: [RouterOutlet, Content],
 })
 export class AppComponent {
-  title = 'v18-angular-app';
+  apiKey = 'ad30f9a246614faaa6a03374f83554c9';
+  model = 'page';
+  content: BuilderContent | null = null;
+
+  async ngOnInit() {
+    const urlPath = window.location.pathname || '';
+
+    const content = await fetchOneEntry({
+      apiKey: this.apiKey,
+      model: this.model,
+      userAttributes: {
+        urlPath: '/angular-123',
+      },
+    });
+
+    if (!content) {
+      return;
+    }
+
+    this.content = content;
+  }
 }
